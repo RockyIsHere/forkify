e9bdc3771adec6ba18d71dc652738354f79f0bfc
RockyIsHere <rocky.mld2016@gmail.com>
2021-09-17 17:22:10 +0530

    initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'ada95409-9108-4b68-9ef6-a06fcee941b2';
+export const MODAL_CLOSE_SEC = 2.5;
